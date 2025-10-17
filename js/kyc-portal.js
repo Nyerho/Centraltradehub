884af59abecc8f1e3b85809a9f49888f188e35b8
User <user@example.com>
2025-10-17 07:12:43 +0100

    last fix

--- js/kyc-portal.js
+++ js/kyc-portal.js
@@ -203,10 +203,8 @@ function setKycImagePreview(imgEl, fileUrl) {
   imgEl.classList.add('kyc-image-preview'); // ensure sizing rules apply
 }
 
-// Ensure we have required imports
-import { auth, db } from "./firebase-config.js";
-import { onAuthStateChanged } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
-import { doc, onSnapshot } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
+
+
 
 // Force disable image previews and show only filenames
 function disableKycImagePreview() {
